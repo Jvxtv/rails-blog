bb8539c8ee80d0507b4b250a3b93e939ced15b95
Luís Ferreira <zamith.28@gmail.com>
2014-11-28 18:45:27 +0000

    Change callback name
    A method like `role` would be expected to return a role (a getter). A more idiomatic name for this callback is `set_role`. Also, there is no need to use the lower level methods such as `write_attribute`.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ require_relative 'user_roles'
 class User < ActiveRecord::Base
   include Clearance::User
 
-  before_save :role
+  before_save :set_role
 
   validates :password, length: { minimum: 4 }
   validates :email, presence: true, uniqueness: true
   validates :role, presence: true, inclusion: { in: UserRoles.all }
 
-  def role
-    write_attribute(:role, read_attribute(:role) || UserRoles.initial_role)
+  def set_role
+    self.role ||= UserRoles.initial_role
   end
 
   def admin?
