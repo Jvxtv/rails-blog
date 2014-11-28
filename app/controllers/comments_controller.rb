06969e1cb54def0557596ff1898ab39ab4f92212
Luís Ferreira <zamith.28@gmail.com>
2014-11-28 22:07:47 +0000

    Corrects typo
    Also, no need for the comment to be an instance variable, since it is being destroyed and thus no rendered in the view.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,8 +10,8 @@ class CommentsController < ApplicationController
 
   def destroy
     @article = Article.find(params[:article_id])
-    @commnet = @article.comments.find(params[:id])
-    @commnet.destroy
+    comment = @article.comments.find(params[:id])
+    comment.destroy
     redirect_to article_path(@article)
   end
 
